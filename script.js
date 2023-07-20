594af07a03b46384d6e16c617ee2651ab89dcd13
vildanc <cvildan.32@gmail.com>
2023-07-20 14:07:28 +0300

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Resimlerin büyüdüğü ve sayfanın geri kalanının saydamlaştığı fonksiyon
+function expandImage(event) {
+    const image = event.target;
+    const expandedImage = document.createElement("img");
+    expandedImage.src = image.src;
+    expandedImage.alt = image.alt;
+    expandedImage.classList.add("expanded-image");
+  
+    const transparentBackground = document.createElement("div");
+    transparentBackground.classList.add("transparent-background");
+    transparentBackground.appendChild(expandedImage);
+  
+    document.body.appendChild(transparentBackground);
+  
+    transparentBackground.addEventListener("click", function () {
+      document.body.removeChild(transparentBackground);
+    });
+  }
+  
+  // Tüm resimlere tıklama olayını ekleyin
+  const imageWrappers = document.querySelectorAll(".hex-column img");
+  imageWrappers.forEach(function (wrapper) {
+    wrapper.addEventListener("click", expandImage);
+  });
+  
